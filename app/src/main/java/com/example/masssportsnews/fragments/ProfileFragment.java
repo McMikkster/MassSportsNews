5144ee516d7ad807f62f4c95f92ddd6c2c895357
soniqueee <sonique_eccles@bloomfield.edu>
2022-04-20 21:40:44 -0400

    profile updated

--- app/src/main/java/com/example/masssportsnews/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/masssportsnews/fragments/ProfileFragment.java
@@ -8,30 +8,31 @@ import androidx.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.material.button.MaterialButton;
+import com.example.masssportsnews.adapter.ProfileAdapter;
+import com.example.models.Profile;
 
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.masssportsnews.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProfileFragment extends Fragment {
 
-    private TextView tvProfile;
-    private TextView viewFirstName;
-    private TextView viewLastName;
-    private TextView viewEmail;
-    private TextView viewPhoneNumber;
-    private TextView viewPassword;
-    private TextView viewAddress;
-    private MaterialButton btnEditProfile;
+    //private TextView tvProfile;
+    //private TextView viewFirstName;
+    //private TextView viewLastName;
+    //private TextView viewEmail;
+    //private TextView viewPhoneNumber;
+    //private TextView viewPassword;
+    //private TextView viewAddress;
+    //private MaterialButton btnEditProfile;
+
+    ProfileAdapter profileAdapter;
+    List<Profile> profileList;
 
     public ProfileFragment()
     {
@@ -42,30 +43,19 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.item_profile, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        tvProfile = view.findViewById(R.id.tvProfile);
-        viewFirstName = view.findViewById(R.id.viewFirstName);
-        viewLastName = view.findViewById(R.id.viewLastName);
-        viewEmail = view.findViewById(R.id.viewEmail);
-        viewPhoneNumber = view.findViewById(R.id.viewPhoneNumber);
-        viewPassword = view.findViewById(R.id.viewPassword);
-        viewAddress = view.findViewById(R.id.viewAddress);
-        btnEditProfile = view.findViewById(R.id.btnEditProfile);
-
-        btnEditProfile.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
+    profileList = new ArrayList<>();
+    profileAdapter = new ProfileAdapter(getContext(), profileList);
 
 
-            }
-        });
+
+
+
     }
 }
