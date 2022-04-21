f52bda17508d9d4dedf423efc4fac5a5e5dc6e99
soniqueee <sonique_eccles@bloomfield.edu>
2022-04-21 11:47:40 -0400

    profile updated

--- app/src/main/java/com/example/masssportsnews/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/masssportsnews/fragments/ProfileFragment.java
@@ -1,20 +1,35 @@
 package com.example.masssportsnews.fragments;
 
+import static com.example.models.Profile.KEY_ADDRESS;
+import static com.example.models.Profile.KEY_EMAIL;
+import static com.example.models.Profile.KEY_FIRSTNAME;
+import static com.example.models.Profile.KEY_LASTNAME;
+import static com.example.models.Profile.KEY_PHONENUMBER;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.masssportsnews.adapter.ProfileAdapter;
 import com.example.models.Profile;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.masssportsnews.R;
+
+import com.google.android.material.button.MaterialButton;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,15 +37,18 @@ import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
-    //private TextView tvProfile;
-    //private TextView viewFirstName;
-    //private TextView viewLastName;
-    //private TextView viewEmail;
-    //private TextView viewPhoneNumber;
-    //private TextView viewPassword;
-    //private TextView viewAddress;
-    //private MaterialButton btnEditProfile;
+    public static final String TAG = "ProfileFragment";
 
+    private TextView tvProfile;
+    private TextView viewFirstName;
+    private TextView viewLastName;
+    private TextView viewEmail;
+    private TextView viewPhoneNumber;
+    private TextView viewPassword;
+    private TextView viewAddress;
+    MaterialButton btnEditProfile;
+
+    RecyclerView rvProfile;
     ProfileAdapter profileAdapter;
     List<Profile> profileList;
 
@@ -43,16 +61,57 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.item_profile, container, false);
+        return inflater.inflate(R.layout.profile_fragment, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+//        viewFirstName = view.findViewById(R.id.viewFirstName);
+//        viewLastName = view.findViewById(R.id.viewLastName);
+//        viewEmail = view.findViewById(R.id.viewEmail);
+//        viewPhoneNumber = view.findViewById(R.id.viewEmail);
+//        viewAddress = view.findViewById(R.id.viewAddress);
+        rvProfile = view.findViewById(R.id.rvprofile);
     profileList = new ArrayList<>();
     profileAdapter = new ProfileAdapter(getContext(), profileList);
+    rvProfile.setAdapter(profileAdapter);
+    rvProfile.setLayoutManager(new LinearLayoutManager(getContext()));
 
+    queryProfile();
+
+    }
+
+    private void queryProfile() {
+
+        ParseQuery<Profile> query = ParseQuery.getQuery(Profile.class);
+        query.include(KEY_FIRSTNAME);
+        query.include(KEY_LASTNAME);
+        query.include(KEY_EMAIL);
+        query.include (KEY_PHONENUMBER);
+        query.include(KEY_ADDRESS);
+
+        query.findInBackground(new FindCallback<Profile>() {
+            @Override
+            public void done(List<Profile>  profile, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "issue with profile", e);
+                    return;
+                }
+//                for(Profile profile: profile){
+//                    Log
+//                }
+//
+
+
+                profileList.addAll(profile);
+                profileAdapter.notifyDataSetChanged();
+
+
+            }
+
+        });
 
 
 
